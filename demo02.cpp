eb679705f899289671650ff382665bc80f218ff7
Mahatoshilpi <shilpimahato05@gmail.com>
2025-03-27 23:07:13 +0530

    datatypes of c++

---
+++ demo02.cpp
@@ -0,0 +1,23 @@
+//datatypes
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    //int 
+    int a=10;
+
+    //long datatype has a wider range than integer
+    //long long will give even a wider number range
+    long long x=1500000000;
+    //float ,double
+    float p=13.5;
+
+    //string, get line
+
+
+    string str;
+    getline(cin,str);
+    cout<<str;
+    return 0;
+
+
+}
